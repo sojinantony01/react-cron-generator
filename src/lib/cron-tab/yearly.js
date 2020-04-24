d56da6892673fd4231365743dd9d53bf7aef77d5
sojin <sojin.antony@simplogics.com>
2020-04-24 09:04:39 +0530

    component name change

--- src/lib/cron-tab/yearly.js
+++ src/lib/cron-tab/yearly.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default  class Yearly extends Component {
+export default  class YearlyCron extends Component {
     constructor(props) {
         super(props);
         this.state = {
