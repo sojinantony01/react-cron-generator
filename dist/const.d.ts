08bb6e577bd080d384c1ff350b419bab4146d7f4
Denis Ilguzin <352586+ilguzin@users.noreply.github.com>
2020-11-20 11:08:00 +0300

    Fixes

--- dist/const.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const MINUTE_POSITION_INDEX = 0;
-export declare const HOUR_POSITION_INDEX = 1;
-export declare const DAY_OF_MONTH_POSITION_INDEX = 2;
-export declare const MONTH_POSITION_INDEX = 3;
-export declare const DAY_OF_WEEK_POSITION_INDEX = 4;
--- dist/const.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DAY_OF_WEEK_POSITION_INDEX = exports.MONTH_POSITION_INDEX = exports.DAY_OF_MONTH_POSITION_INDEX = exports.HOUR_POSITION_INDEX = exports.MINUTE_POSITION_INDEX = void 0;
-exports.MINUTE_POSITION_INDEX = 0;
-exports.HOUR_POSITION_INDEX = 1;
-exports.DAY_OF_MONTH_POSITION_INDEX = 2;
-exports.MONTH_POSITION_INDEX = 3;
-exports.DAY_OF_WEEK_POSITION_INDEX = 4;
-//# sourceMappingURL=const.js.map
