018117e831d4a077a2db1ad24e6499be8f55ad75
Sojin antony <sojin.antony01@gmail.com>
2022-09-19 11:29:35 +0530

    Create gpr-hack.js

---
+++ gpr-hack.js
@@ -0,0 +1,12 @@
+// gpr-hack.js
+import { writeFileSync, readFileSync } from "fs";
+
+const file = readFileSync("./package.json", {
+  encoding: "utf-8",
+});
+
+const json = JSON.parse(file);
+
+json.name = "@sojinantony01/react-cron-generator";
+
+writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
