e9243f279f1add669c000bf6ee9c7278baded064
Sojin antony <sojin.antony01@gmail.com>
2024-12-31 13:45:14 +0530

    CronProp and cronsture importable (#82)
    * CronProp importable
    
    * Make cronsture importable #81

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
-import Cron from './cron';
+import Cron, { CronProp } from "./cron";
 import { HEADER } from './meta';
+import cronstrue from "cronstrue/i18n";
 
-export { HEADER };
+export { HEADER, cronstrue };
+export type { CronProp };
 export default Cron;
