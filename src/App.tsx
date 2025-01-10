e77da57410b951ebba5048d0aa289412c4d58e1b
Shivam <shivam178sharma@gmail.com>
2025-01-10 18:09:30 +0530

    Add prettier formatter and pull request template (#84)
    * Add prettier formatter (#1)
    
    * undo dependencies and workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import React, { useState } from 'react';
 import Cron from './lib';
 import { HEADER } from './lib';
-import  packageConf from  '../package.json'
+import packageConf from '../package.json';
 
-const cronOptions  = {
+const cronOptions = {
   headers: [HEADER.CUSTOM, HEADER.MINUTES, HEADER.HOURLY, HEADER.WEEKLY, HEADER.MONTHLY],
 };
 
 interface State {
-  value?: string
+  value?: string;
 }
 function App() {
-  const [state, setState] = useState<State>({})
+  const [state, setState] = useState<State>({});
 
   return (
     <div>
-      <div>
-        React cron generator: V {packageConf.version}
-      </div>
+      <div>React cron generator: V {packageConf.version}</div>
       <div>
         <Cron
-          onChange={(e, text)=> {setState({value:e}); console.log(e, text)}}
+          onChange={(e, text) => {
+            setState({ value: e });
+            console.log(e, text);
+          }}
           value={state.value}
           showResultText={true}
           showResultCron={true}
           // options={cronOptions}
-          />  
-      </div>               
+        />
+      </div>
     </div>
   );
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
-import Cron, { CronProp } from "./cron";
+import Cron, { CronProp } from './cron';
 import { HEADER } from './meta';
-import cronstrue from "cronstrue/i18n";
+import cronstrue from 'cronstrue/i18n';
 
 export { HEADER, cronstrue };
 export type { CronProp };
