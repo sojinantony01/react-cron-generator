47813f335d79465375383d7e3d336605e78c5e5c
sojin antony <sojin.antony01@gmail.com>
2025-12-29 23:11:35 +0530

    lint fixes, dependencies update, change to vite

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-cron-generator/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
+
+
